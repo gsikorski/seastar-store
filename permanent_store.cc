af3813a66e6771ae1448d096e2132b7550bf5729
Grzegorz Sikorski <grzegorz.sikorski@us.abb.com>
2024-09-30 20:39:17 +0200

    Replace fstream with non-blocking seastar file access

--- permanent_store.cc
+++ permanent_store.cc
@@ -1,6 +1,6 @@
 #include "permanent_store.h"
-#include <fstream>
 #include <nlohmann/json.hpp>
+#include <seastar/core/file.hh>
 #include <seastar/core/smp.hh>
 
 using json = nlohmann::json;
@@ -9,14 +9,45 @@ namespace store {
 
 namespace {
 
-auto load(const std::filesystem::path shard_file) {
-  std::ifstream file(shard_file);
-  return json::parse(file);
+constexpr static auto PAGE_SIZE = 4096;
+constexpr static auto MAX_SHARD_SIZE = PAGE_SIZE * std::kilo::num;
+
+auto read_file(const std::filesystem::path filename,
+               seastar::temporary_buffer<char> &rbuf) {
+  return seastar::with_file(
+      seastar::open_file_dma(std::string{filename}, seastar::open_flags::ro),
+      [&rbuf](seastar::file &file) {
+        return file.dma_read(0, rbuf.get_write(), PAGE_SIZE);
+      });
 }
 
-void store(const std::filesystem::path shard_file, const json &content) {
-  std::ofstream file(shard_file);
-  file << content;
+auto load(const std::filesystem::path shard_file) -> seastar::future<json> {
+  auto rbuf =
+      seastar::temporary_buffer<char>::aligned(PAGE_SIZE, MAX_SHARD_SIZE);
+  co_await read_file(shard_file, rbuf);
+  co_return json::parse(rbuf);
+}
+
+auto write_file(const std::filesystem::path filename,
+                seastar::temporary_buffer<char> &wbuf, size_t buffer_size) {
+  return seastar::with_file(
+      seastar::open_file_dma(std::string{filename},
+                             seastar::open_flags::rw |
+                                 seastar::open_flags::create),
+      [&wbuf, buffer_size](seastar::file &file) {
+        return file.dma_write(0, wbuf.get(), MAX_SHARD_SIZE);
+      });
+};
+
+seastar::future<> store(const std::filesystem::path shard_file,
+                        const json &content) {
+  auto wbuf =
+      seastar::temporary_buffer<char>::aligned(PAGE_SIZE, MAX_SHARD_SIZE);
+  const auto json_str = content.dump();
+  assert(json_str.size() < MAX_SHARD_SIZE);
+  std::fill(wbuf.get_write(), wbuf.get_write() + MAX_SHARD_SIZE, 0);
+  std::ranges::copy(json_str, wbuf.get_write());
+  co_await write_file(shard_file, wbuf, json_str.size());
 }
 
 } // namespace
@@ -28,26 +59,28 @@ permanent_store::permanent_store() {
 seastar::future<> permanent_store::update(const std::string &key,
                                           const std::string &value) {
   auto shard_id = std::hash<std::string>{}(key) % seastar::smp::count;
-  return seastar::smp::submit_to(shard_id, [shard_id, key, value] {
-    const auto fname = fmt::format("storage/{}.json", shard_id);
-    auto loaded = std::filesystem::exists(fname) ? load(fname) : json{};
-    loaded[key] = std::move(value);
-    store(fname, loaded);
-    return;
-  });
+  return seastar::smp::submit_to(
+      shard_id, [shard_id, key, value]() -> seastar::future<> {
+        const auto fname = fmt::format("storage/{}.json", shard_id);
+        auto loaded = json{};
+        if (std::filesystem::exists(fname))
+          loaded = co_await load(fname);
+        loaded[key] = std::move(value);
+        co_await store(fname, loaded);
+      });
 }
 
 seastar::future<> permanent_store::erase(const std::string &key) {
   auto shard_id = std::hash<std::string>{}(key) % seastar::smp::count;
-  return seastar::smp::submit_to(shard_id, [shard_id, key] {
-    const auto fname = fmt::format("storage/{}.json", shard_id);
-    if (not std::filesystem::exists(fname))
-      return;
-    auto loaded = load(fname);
-    loaded.erase(key);
-    store(fname, loaded);
-    return;
-  });
+  return seastar::smp::submit_to(
+      shard_id, [shard_id, key] -> seastar::future<> {
+        const auto fname = fmt::format("storage/{}.json", shard_id);
+        if (not std::filesystem::exists(fname))
+          co_return;
+        auto loaded = co_await load(fname);
+        loaded.erase(key);
+        co_await store(fname, loaded);
+      });
 }
 
 seastar::future<std::optional<std::string>>
@@ -58,7 +91,7 @@ permanent_store::find_one(const std::string &key) {
         const auto fname = fmt::format("storage/{}.json", shard_id);
         if (not std::filesystem::exists(fname))
           co_return std::optional<std::string>{};
-        auto loaded = load(fname);
+        auto loaded = co_await load(fname);
         if (not loaded.contains(key))
           co_return std::optional<std::string>{};
         co_return loaded[key];
@@ -73,7 +106,7 @@ seastar::future<std::string> permanent_store::find_all() {
         auto partial = co_await seastar::smp::submit_to(
             shard_id, [shard_id] -> seastar::future<json> {
               const auto fname = fmt::format("storage/{}.json", shard_id);
-              co_return std::filesystem::exists(fname) ? load(fname)
+              co_return std::filesystem::exists(fname) ? co_await load(fname)
                                                        : json::object();
             });
         for (auto it = partial.begin(); it != partial.end(); ++it)
